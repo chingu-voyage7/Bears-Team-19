db99c03d9084db8920662eadfa060042cd058347
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2018-12-09 21:18:56 +0100

    add signup and login routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,38 +1,64 @@
 const express = require('express')
 const bcrypt = require('bcrypt-nodejs')
 const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const client = require('../client')
+
+require('../config/passport')(passport)
 
 const router = express.Router()
-const db = []
+const db = [1, 2, 3]
 
 const encryptPassword = password => bcrypt.hashSync(password)
 
-/* GET users listing. */
-router.get('/profile/:id', (req, res) => {
-  res.json(db)
+router.get(
+  '/profile/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json(db)
+  },
+)
+
+router.get('/', (req, res) => {
+  client
+    .getUsers('/users')
+    .then(result => res.json(result))
+    .catch(error => res.json({ error }))
 })
 
 router.post('/signup', (req, res) => {
-  const { email, name, password } = req.body
+  const { email, name, username, password } = req.body
   const encryptedPassword = encryptPassword(password)
-  const details = { email, encryptedPassword, name }
-  db.push(details)
-  res.json({ success: true, db })
+  const details = { email, name, username }
+  client
+    .postUser('/users', details)
+    .then(result => res.json(result))
+    .catch(error => res.json({ error }))
 })
 
 router.post('/signin', (req, res) => {
   const { email, password } = req.body
-  const user = db.filter(u => u.email === email)[0]
-  const isValid = user
-    ? bcrypt.compareSync(password, user.encryptedPassword)
-    : false
+  client
+    .getUsers('/users')
+    .then(result => {
+      const user = result.data.filter(item => item.email === email)[0]
 
-  if (isValid) {
-    const token = jwt.sign({ email: user.email, name: user.name }, 'bears19')
-    res.json(token)
-  } else {
-    res.status(400).json(`Wrong credentials!`)
-  }
+      const isValid = user
+        ? bcrypt.compareSync(password, user.encryptedPassword)
+        : false
+
+      if (isValid) {
+        const token = jwt.sign(
+          { email: user.email, name: user.name, id: user.id },
+          process.env.TOKEN_SECRET,
+          { expiresIn: 86400 * 7 },
+        )
+        res.json(token)
+      } else {
+        res.status(400).json({ msg: `Wrong credentials!` })
+      }
+    })
+    .catch(error => res.json({ error }))
 })
 
 module.exports = router
