c815372b77bd168183bd52c79a0cf312ef72e86c
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2018-12-15 11:21:00 +0100

    clean up

--- server/app.js
+++ server/app.js
@@ -17,7 +17,6 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
 app.use('/users', users)
