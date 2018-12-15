bb3a06ff2a69251738d0ad0c03ccab62ab351340
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2018-12-15 11:19:02 +0100

    add auth header to client

--- server/client.js
+++ server/client.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 
 axios.defaults.baseURL = process.env.DB_URL
+axios.defaults.headers.common.Authorization = `Bearer ${process.env.API_TOKEN}`
 
 module.exports = {
   postUser: (url, payload) => axios.post(url, payload),
