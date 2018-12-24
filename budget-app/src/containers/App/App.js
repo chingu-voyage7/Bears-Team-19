19fa952dbc5a161e6767d4e43906420df1034527
Daily120 <forzhacker@mail.ru>
2018-12-24 12:27:07 +0100

    imported bulma into index.js

--- budget-app/src/containers/App/App.js
+++ budget-app/src/containers/App/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   constructor() {
-    super();
-    this.state = {
-        
-    }
+    super()
+    this.state = {}
   }
 
   render() {
     return (
       <div className="App">
         <h1>Welcome Aboard!</h1>
+        <a
+          onMouseEnter={event => event.target.classList.add('is-loading')}
+          className="button is-large"
+          href=""
+        >
+          Bulma Test
+        </a>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
