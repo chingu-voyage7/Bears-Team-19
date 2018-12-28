3b4d8bde154c9a4b1160860c0fc00decdddb08d4
osycon <fredrichawk@gmail.com>
2018-12-28 07:20:37 +0100

    Added react router and just a basic routing setup

---
+++ budget-app/src/containers/About/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About = () => {
+  return <div>This is About!</div>
+}
+
+export default About
---
+++ budget-app/src/containers/Home/Home.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Home = () => <div>This is Home!</div>
+
+export default Home
