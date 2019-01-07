3b70795bc5839123f2d8909d81ddcd88f198839d
osycon <fredrichawk@gmail.com>
2019-01-07 19:19:31 +0100

    Added features section.

--- budget-app/src/containers/Home/Home.js
+++ budget-app/src/containers/Home/Home.js
@@ -2,14 +2,76 @@ import React from 'react'
 import './Home.css'
 
 const Home = () => (
-  <section className="above-fold">
-    <h2>Budgeting app for your every day use.</h2>
-    <p className="tagline">
-      Far far away, behind the word mountains, far from the countries Vokalia
-      and Consonantia, there live the blind texts. Separated
-    </p>
-    <button className="button is-success">Join us</button>
-  </section>
+  <>
+    <section className="above-fold">
+      <h2>Budgeting app for your every day use.</h2>
+      <p className="tagline">
+        Far far away, behind the word mountains, far from the countries Vokalia
+        and Consonantia, there live the blind texts. Separated
+      </p>
+      <button className="button is-success">Join us</button>
+    </section>
+    <section className="features">
+      <h2>Features</h2>
+      <article className="feature">
+        <h3>Feature 1</h3>
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they
+          live in Bookmarksgrove right at the coast of the Semantics, a large
+          language ocean. A small river named Duden
+        </p>
+      </article>
+      <article className="feature">
+        <h3>Feature 2</h3>
+        <img
+          src="https://dummyimage.com/600x400/a3a3a3/fff.jpg"
+          alt="placeholder"
+        />
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they
+          live in Bookmarksgrove right at the coast of the Semantics, a large
+          language ocean. A small river named Duden
+        </p>
+      </article>
+      <article className="feature">
+        <h3>Feature 3</h3>
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they
+          live in Bookmarksgrove right at the coast of the Semantics, a large
+          language ocean. A small river named Duden
+        </p>
+      </article>
+      <article className="feature">
+        <h3>Feature 4</h3>
+        <img
+          src="https://dummyimage.com/600x400/a3a3a3/fff.jpg"
+          alt="placeholder"
+        />
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they
+          live in Bookmarksgrove right at the coast of the Semantics, a large
+          language ocean. A small river named Duden
+        </p>
+      </article>
+      <article className="feature">
+        <img
+          src="https://dummyimage.com/600x400/a3a3a3/fff.jpg"
+          alt="placeholder"
+        />
+        <h3>Feature 5</h3>
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they
+          live in Bookmarksgrove right at the coast of the Semantics, a large
+          language ocean. A small river named Duden
+        </p>
+      </article>
+    </section>
+  </>
 )
 
 export default Home
