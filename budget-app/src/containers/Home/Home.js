a25d48c8a7a8d229b38d4a71259f205edb603bfd
osycon <fredrichawk@gmail.com>
2019-01-07 19:13:14 +0100

    Added elements for above the fold landing page.

--- budget-app/src/containers/Home/Home.js
+++ budget-app/src/containers/Home/Home.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import './Home.css'
 
-const Home = () => <div>This is Home!</div>
+const Home = () => (
+  <section className="above-fold">
+    <h2>Budgeting app for your every day use.</h2>
+    <p className="tagline">
+      Far far away, behind the word mountains, far from the countries Vokalia
+      and Consonantia, there live the blind texts. Separated
+    </p>
+    <button className="button is-success">Join us</button>
+  </section>
+)
 
 export default Home
