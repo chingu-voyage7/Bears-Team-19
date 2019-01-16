6afb699ad6194508645cc38fcc696aeadccb99fd
osycon <fredrichawk@gmail.com>
2019-01-16 19:25:32 +0100

    Connected Redux to the project.

--- budget-app/src/index.js
+++ budget-app/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import 'bulma/css/bulma.css'
 import './index.css'
 import App from './containers/App/App'
+import rootReducer from './store/reducers'
+
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ budget-app/src/store/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  users: [
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Greg' },
+    { id: 3, name: 'James' },
+  ],
+}
+
+const authReducer = (state = initState, action) => {
+  return state
+}
+
+export default authReducer
