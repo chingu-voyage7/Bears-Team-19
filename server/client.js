03af2eb565093b6dc881c580005c766da2d6cb82
andela-uopaluwa <ufedojo.opaluwa@andela.com>
2019-01-22 13:51:12 +0100

    add income and expense client methods

--- server/client.js
+++ server/client.js
@@ -6,4 +6,12 @@ axios.defaults.headers.common.Authorization = `Bearer ${process.env.API_TOKEN}`
 module.exports = {
   postUser: (url, payload) => axios.post(url, payload),
   getUsers: url => axios.get(url),
+  postIncome: (url, payload) => axios.post(url, payload),
+  postExpense: (url, payload) => axios.post(url, payload),
+  getIncome: url => axios.get(url),
+  getExpense: url => axios.get(url),
+  deleteExpense: url => axios.delete(url),
+  deleteIncome: url => axios.delete(url),
+  updateIncome: (url, payload) => axios.patch(url, payload),
+  updateExpense: (url, payload) => axios.patch(url, payload),
 }
