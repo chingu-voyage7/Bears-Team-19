565f73ae6c0ba698c50adc1e203c4e4d040f62b9
osycon <fredrichawk@gmail.com>
2019-01-26 09:40:16 +0100

    feat: Dashboard component added.

--- budget-app/src/containers/Dashboard/Dashboard.js
+++ budget-app/src/containers/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import Icon from '../../components/Icons'
+import './Dashboard.css'
 
 class Dashboard extends Component {
   render() {
@@ -9,7 +11,92 @@ class Dashboard extends Component {
     if (!auth.uid) {
       return <Redirect to="/signin" />
     }
-    return <div>Dashboard</div>
+    return (
+      <section className="dashboard">
+        <h3>Dashboard</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>
+                <abbr title="Amount">Amt</abbr>
+              </th>
+              <th>
+                <abbr title="Category">Cat</abbr>
+              </th>
+              <th>
+                <abbr title="Account">Account</abbr>
+              </th>
+              <th>Edit</th>
+              <th>
+                <abbr title="Delete">Del</abbr>
+              </th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>
+                <abbr title="Amount">Amt</abbr>
+              </th>
+              <th>
+                <abbr title="Category">Cat</abbr>
+              </th>
+              <th>
+                <abbr title="Account">Account</abbr>
+              </th>
+              <th>Edit</th>
+              <th>
+                <abbr title="Delete">Del</abbr>
+              </th>
+            </tr>
+          </tfoot>
+          <tbody>
+            <tr>
+              <td>2019-01-12</td>
+              <td>Expense</td>
+              <td>14.23</td>
+              <td>Groceries</td>
+              <td>Spending account</td>
+              <td>
+                <Icon name="edit" color="#6179C7" />
+              </td>
+              <td>
+                <Icon name="delete" color="#E94B25" />
+              </td>
+            </tr>
+            <tr>
+              <td>2019-01-12</td>
+              <td>Income</td>
+              <td>14.23</td>
+              <td>Groceries</td>
+              <td>Spending account</td>
+              <td>
+                <Icon name="edit" color="#6179C7" />
+              </td>
+              <td>
+                <Icon name="delete" color="#E94B25" />
+              </td>
+            </tr>
+            <tr>
+              <td>2019-01-12</td>
+              <td>Expense</td>
+              <td>14.23</td>
+              <td>Groceries</td>
+              <td>Spending account</td>
+              <td>
+                <Icon name="edit" color="#6179C7" />
+              </td>
+              <td>
+                <Icon name="delete" color="#E94B25" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    )
   }
 }
 
