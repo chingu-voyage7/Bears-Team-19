f4ef0451427a2f271f18bdd8e8f987a6c3bc856c
osycon <fredrichawk@gmail.com>
2019-02-03 15:42:52 +0100

    fix: Updated knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,12 @@
 const path = require('path')
+
+require('dotenv').config()
+
 const { POSTGRES_URL_DEV, POSTGRES_URI } = process.env
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: `${POSTGRES_URL_DEV}`,
     migrations: {
       directory: path.join(__dirname, '/migrations'),
@@ -13,7 +16,7 @@ module.exports = {
     },
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: `${POSTGRES_URI}`,
     migrations: {
       directory: path.join(__dirname, '/migrations'),
