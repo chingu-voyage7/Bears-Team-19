600ed06cf549dc3647bbe95818ad35e505120553
osycon <fredrichawk@gmail.com>
2019-02-16 18:57:02 +0100

    refactor: Updated edit form to use Formik and yup for validation.

--- budget-app/src/containers/EditTransaction/EditTransaction.js
+++ budget-app/src/containers/EditTransaction/EditTransaction.js
@@ -1,163 +1,181 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React, { Component } from 'react'
-import DayPicker from 'react-day-picker'
-import 'react-day-picker/lib/style.css'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import { connect } from 'react-redux'
+import * as yup from 'yup'
+import { format, addDays } from 'date-fns'
 import { Redirect } from 'react-router-dom'
 import { updateTransaction } from '../../store/actions/transactionActions'
 import './EditTransaction.css'
 
+const schema = yup.object().shape({
+  amount: yup
+    .number('Value must be a number.')
+    .min(0.01, 'Number has to be higher than 0.')
+    .required('Required'),
+  account: yup.string().required('Required'),
+  category: yup
+    .string()
+    .trim('No whitespace!')
+    .required('Required'),
+  type: yup.string().required('Required'),
+  dateselect: yup
+    .date()
+    .max(addDays(new Date(), 1), 'Can not input future dates.')
+    .required('Required'),
+})
+
 class EditTransaction extends Component {
   state = {
-    amount: this.props.transaction ? this.props.transaction.amount : '',
-    category: this.props.transaction ? this.props.transaction.category : '',
-    account: this.props.transaction ? this.props.transaction.account : '',
-    selectedOption: this.props.transaction ? this.props.transaction.type : '',
-    selectedDay: this.props.transaction
-      ? new Date(this.props.transaction.date)
-      : undefined,
     toDashboard: false,
-  }
-
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value,
-    })
-  }
-
-  handleOptionChange = changeEvent => {
-    this.setState({
-      selectedOption: changeEvent.target.value,
-    })
-  }
-
-  handleDayClick = (day, { selected }) => {
-    if (selected) {
-      this.setState({ selectedDay: undefined })
-      return
-    }
-    this.setState({
-      selectedDay: day,
-    })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    // check so all are selected
-    if (
-      this.state.selectedDay === undefined &&
-      this.state.amount &&
-      this.state.category
-    ) {
-      console.log('Fill in the required fields')
-    }
-    const fields = {
-      uid: this.props.auth.uid,
-      id: this.props.transaction.trans_id,
-      amount: this.state.amount,
-      account: this.state.account,
-      category: this.state.category,
-      type: this.state.selectedOption,
-      date: this.state.selectedDay,
-    }
-    this.props.updateTransaction(fields)
-    this.setState({
-      toDashboard: true,
-    })
   }
   render() {
     if (this.state.toDashboard === true) {
       return <Redirect to="/" />
     }
+    const { amount, category, account, type, date } = this.props.transaction
 
     return (
       <section className="update-transaction">
         <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <h3>Update transaction</h3>
-            <div className="field">
-              <label htmlFor="amount" className="label">
-                Amount
-              </label>
-              <div className="control has-icon-left has-icons-right">
-                <input
-                  type="number"
-                  step="0.01"
-                  className="input"
-                  placeholder="ex 10.03"
-                  required
-                  id="amount"
-                  value={this.state.amount}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <label htmlFor="category" className="label">
-                Category
-              </label>
-              <div className="control has-icon-left has-icons-right">
-                <input
-                  type="text"
-                  className="input"
-                  placeholder="Category"
-                  required
-                  id="category"
-                  value={this.state.category}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <label htmlFor="account" className="label">
-                Account
-              </label>
-              <div className="control has-icon-left has-icons-right">
-                <input
-                  type="text"
-                  className="input"
-                  placeholder="Account"
-                  id="account"
-                  value={this.state.account}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <label htmlFor="income" className="radio">
-                <input
-                  type="radio"
-                  id="income"
-                  name="type"
-                  value="income"
-                  checked={this.state.selectedOption === 'income'}
-                  onChange={this.handleOptionChange}
-                  required
-                />
-                <span>Income</span>
-              </label>
-              <label htmlFor="expense" className="radio">
-                <input
-                  type="radio"
-                  id="expense"
-                  name="type"
-                  value="expense"
-                  checked={this.state.selectedOption === 'expense'}
-                  onChange={this.handleOptionChange}
-                  required
-                />
-                <span>Expense</span>
-              </label>
-            </div>
-            <div className="field">
-              <DayPicker
-                onDayClick={this.handleDayClick}
-                selectedDays={this.state.selectedDay}
-              />
-            </div>
-            <div className="control">
-              <button className="button is-success">Update transaction</button>
-            </div>
-          </form>
+          <Formik
+            initialValues={{
+              amount,
+              category,
+              account,
+              type,
+              dateselect: format(date),
+            }}
+            validationSchema={schema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                const updatedTransaction = {
+                  ...values,
+                  uid: this.props.auth.uid,
+                  id: this.props.transaction.trans_id,
+                }
+                this.props.updateTransaction(updatedTransaction)
+                setSubmitting(false)
+                this.setState({
+                  toDashboard: true,
+                })
+              }, 400)
+            }}
+          >
+            {({ isSubmitting, setFieldValue, values, errors }) => (
+              <Form>
+                <h3>Update transaction</h3>
+                <div className="field">
+                  <label htmlFor="amount" className="label">
+                    Amount
+                    <div className="control">
+                      <Field
+                        type="number"
+                        name="amount"
+                        id="amount"
+                        placeholder="Ex. 12.99"
+                        min="0"
+                        step="0.01"
+                      />
+                    </div>
+                  </label>
+                  <ErrorMessage
+                    name="amount"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="field">
+                  <label htmlFor="category" className="label">
+                    Category
+                    <div className="control">
+                      <Field
+                        type="text"
+                        name="category"
+                        id="category"
+                        placeholder="Ex. Groceries"
+                      />
+                    </div>
+                  </label>
+                  <ErrorMessage
+                    name="category"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="field">
+                  <label htmlFor="account" className="label">
+                    Account
+                    <div className="control">
+                      <Field
+                        type="text"
+                        name="account"
+                        id="account"
+                        placeholder="Ex. Spending account"
+                      />
+                    </div>
+                  </label>
+                  <ErrorMessage
+                    name="account"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <label htmlFor="income" className="label label-radio">
+                      Income
+                      <Field
+                        type="radio"
+                        name="type"
+                        value="income"
+                        id="income"
+                        checked={values.type === 'income'}
+                      />
+                    </label>
+                    <label htmlFor="expense" className="label label-radio">
+                      Expense
+                      <Field
+                        type="radio"
+                        name="type"
+                        value="expense"
+                        id="expense"
+                        checked={values.type === 'expense'}
+                      />
+                    </label>
+                  </div>
+                  <ErrorMessage
+                    name="type"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+                <div className="field">
+                  <label htmlFor="date" className="label">
+                    Date
+                    <div className="control">
+                      <DatePicker
+                        id="date"
+                        name="dateselect"
+                        value={format(values['dateselect'], 'YYYY-MM-DD')}
+                        onChange={e => setFieldValue('dateselect', e)}
+                        maxDate={new Date()}
+                        placeholderText="Click to set the transaction date"
+                        dateFormat="yyyy-MM-dd"
+                      />
+                    </div>
+                  </label>
+                  {errors.dateselect && (
+                    <div className="error-message">{errors.dateselect}</div>
+                  )}
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                  Update Transaction
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </section>
     )
