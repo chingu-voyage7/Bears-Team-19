e7c6b736436429a8c1e8416cbef1a95962e28f07
osycon <fredrichawk@gmail.com>
2019-02-18 19:04:05 +0100

    feat: Added routes for budgets.

---
+++ server/routes/budgets.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const db = require('../database/database.js')
+
+const router = express.Router()
+
+// Get budget
+// Get all budgets
+router.get('/', async (req, res, next) => {
+  // check that user exists
+  const { uid } = req.headers
+  if (!uid) {
+    res.status(404).json({ message: 'Not authorized' })
+  } else {
+    // Check so there is a user in database
+    const userResponse = await db('users')
+      .where({ uid })
+      .select()
+
+    if (!userResponse.length) {
+      res.status(404).json({ message: 'Not authorized' })
+    } else {
+      res.json({ message: 'Budgets' })
+    }
+  }
+  // Get all budgets for that user.
+})
+
+// Create budget
+router.post('/', async (req, res, next) => {
+  // check that user exists
+  const { uid } = req.headers
+  if (!uid) {
+    res.status(404).json({ message: 'Not authorized' })
+  } else {
+    // Check so there is a user in database
+    const userResponse = await db('users')
+      .where({ uid })
+      .select()
+
+    if (!userResponse.length) {
+      res.status(404).json({ message: 'Not authorized' })
+    } else {
+      res.json({ message: 'Budgets Post' })
+    }
+  }
+})
+
+// Update budget
+router.patch('/', async (req, res, next) => {
+  // check that user exists
+  const { uid } = req.headers
+  if (!uid) {
+    res.status(404).json({ message: 'Not authorized' })
+  } else {
+    // Check so there is a user in database
+    const userResponse = await db('users')
+      .where({ uid })
+      .select()
+
+    if (!userResponse.length) {
+      res.status(404).json({ message: 'Not authorized' })
+    } else {
+      res.json({ message: 'Budgets Patch' })
+    }
+  }
+})
+
+// Delete budget
+router.delete('/', async (req, res, next) => {
+  // check that user exists
+  const { uid } = req.headers
+  if (!uid) {
+    res.status(404).json({ message: 'Not authorized' })
+  } else {
+    // Check so there is a user in database
+    const userResponse = await db('users')
+      .where({ uid })
+      .select()
+
+    if (!userResponse.length) {
+      res.status(404).json({ message: 'Not authorized' })
+    } else {
+      res.json({ message: 'Budgets Delete' })
+    }
+  }
+})
+
+module.exports = router
