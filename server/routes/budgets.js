1983f314cf72ea07d8d92fbe13a6e0d120181f03
osycon <fredrichawk@gmail.com>
2019-02-19 14:54:10 +0100

    refactor: Updated budgets to use auth middleware.

--- server/routes/budgets.js
+++ server/routes/budgets.js
@@ -1,111 +1,51 @@
 const express = require('express')
+const { isAuthenticated } = require('../middleware/authMiddleware')
 const db = require('../database/database.js')
 
 const router = express.Router()
 
-// Get budget
-// Get all budgets
-router.get('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
-
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const [{ user_id }] = userResponse
-
-      // Get all budgets for that user.
-      const budgets = await db('budgets')
-        .select()
-        .where({ fk_user_id: user_id })
-      res.json({ message: 'Budgets', budgets })
-    }
-  }
+router.get('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  // Get all budgets for that user.
+  const budgets = await db('budgets')
+    .select()
+    .where({ fk_user_id: userId })
+  res.json({ message: 'Budgets', budgets })
 })
 
 // Create budget
-router.post('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.post('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  const { budgetName } = req.body
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const { budgetName } = req.body
-      const [{ user_id }] = userResponse
+  const newBudget = await db('budgets')
+    .insert({ budget_name: budgetName, fk_user_id: userId })
+    .returning(['budget_name', 'budget_id'])
 
-      const newBudget = await db('budgets')
-        .insert({ budget_name: budgetName, fk_user_id: user_id })
-        .returning(['budget_name', 'budget_id'])
-
-      res.json({ message: 'Budget created', newBudget })
-    }
-  }
+  res.json({ message: 'Budget created', newBudget })
 })
 
 // Update budget
-router.patch('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.patch('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  const { budgetId, budgetName } = req.body
+  const updatedBudget = await db('budgets')
+    .update({ budget_name: budgetName })
+    .where({ budget_id: budgetId, fk_user_id: userId })
+    .returning(['budget_id', 'budget_name'])
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const { budgetId, budgetName } = req.body
-      const updatedBudget = await db('budgets')
-        .update({ budget_name: budgetName })
-        .where({ budget_id: budgetId })
-        .returning(['budget_id', 'budget_name'])
-
-      res.json({ message: 'Budget updated', updatedBudget })
-    }
-  }
+  res.json({ message: 'Budget updated', updatedBudget })
 })
 
 // Delete budget
-router.delete('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.delete('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  const { budgetId: budget_id } = req.body
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const { budgetId: budget_id } = req.body
-
-      const deletedBudget = await db('budgets')
-        .del()
-        .where({ budget_id })
-      res.json({ message: 'Budget deleted', deletedBudget })
-    }
-  }
+  const deletedBudget = await db('budgets')
+    .del()
+    .where({ budget_id, fk_user_id: userId })
+  res.json({ message: 'Budget deleted', deletedBudget })
 })
 
 module.exports = router
