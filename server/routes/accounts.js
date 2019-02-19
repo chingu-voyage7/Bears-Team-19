6362ada15d0eb7d803d4858276994a963fdcd211
osycon <fredrichawk@gmail.com>
2019-02-19 14:49:26 +0100

    refactor: Accounts now use auth middleware.

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { isAuthenticated } = require('../middleware/authMiddleware')
 const db = require('../database/database.js')
 
 const router = express.Router()
@@ -6,116 +7,60 @@ const router = express.Router()
 // Get account
 
 // Get all accounts
-router.get('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.get('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  // Get all accounts for that user.
+  const accounts = await db('accounts')
+    .select()
+    .where({ fk_user_id: userId })
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const [{ user_id }] = userResponse
-
-      // Get all accounts for that user.
-      const accounts = await db('accounts')
-        .select()
-        .where({ fk_user_id: user_id })
-
-      res.json({ message: 'Accounts', accounts })
-    }
-  }
+  res.json({ message: 'Accounts', accounts })
 })
 
 // Create account
-router.post('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.post('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  const { account, balance } = req.body
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const { account, balance } = req.body
-      const [{ user_id }] = userResponse
+  // Create account
+  const newAccount = await db('accounts')
+    .returning(['account_id'])
+    .insert({ fk_user_id: userId, account_name: account, balance })
 
-      // Create account
-      const newAccount = await db('accounts')
-        .returning(['account_id'])
-        .insert({ fk_user_id: user_id, account_name: account, balance })
-
-      res.json({
-        message: 'Accounts created',
-        newAccount,
-      })
-    }
-  }
+  res.json({
+    message: 'Accounts created',
+    newAccount,
+  })
 })
 
 // Update account
-router.patch('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.patch('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  // Update account
+  const { accountId, accountName, balance } = req.body
+  const updatedAccount = await db('accounts')
+    .update({ account_name: accountName, balance })
+    .where({ account_id: accountId, fk_user_id: userId })
+    .returning(['account_id', 'account_name', 'balance'])
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      // Update account
-      const { accountId, accountName, balance } = req.body
-      const updatedAccount = await db('accounts')
-        .update({ account_name: accountName, balance })
-        .where({ account_id: accountId })
-        .returning(['account_id', 'account_name', 'balance'])
-
-      res.json({
-        message: 'Account updated',
-        updatedAccount,
-      })
-    }
-  }
+  res.json({
+    message: 'Account updated',
+    updatedAccount,
+  })
 })
 
 // Delete account
-router.delete('/', async (req, res, next) => {
-  // check that user exists
-  const { uid } = req.headers
-  if (!uid) {
-    res.status(404).json({ message: 'Not authorized' })
-  } else {
-    // Check so there is a user in database
-    const userResponse = await db('users')
-      .where({ uid })
-      .select()
+router.delete('/', isAuthenticated, async (req, res, next) => {
+  const { userId } = req
+  const { accountId } = req.body
 
-    if (!userResponse.length) {
-      res.status(404).json({ message: 'Not authorized' })
-    } else {
-      const { account_id } = req.body
-
-      const deletedAccount = await db('accounts')
-        .del()
-        .where({ account_id })
-      res.json({ message: 'Account deleted', deletedAccount })
-    }
-  }
+  const deletedAccount = await db('accounts')
+    .del()
+    .where({ account_id: accountId, fk_user_id: userId })
+  res.json({
+    message: 'Account deleted',
+    deletedAccount,
+  })
 })
 
 module.exports = router
