f6603c81dd01408e418a1c58f2254f9abff97d42
osycon <fredrichawk@gmail.com>
2019-02-20 14:08:47 +0100

    refactor: Broke out totalBalance calculation.

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -15,4 +15,23 @@ const isValidBudgetAndAccount = async (accountId, budgetId, userId) => {
   return true
 }
 
-module.exports = { isValidBudgetAndAccount }
+const getTotalBalance = async userId => {
+  // Get all accounts for that user
+  const accounts = await db('accounts')
+    .select()
+    .where({ fk_user_id: userId })
+
+  // Get all accounts for that user, if none, default to 0.00
+  // Set total balance
+  let totalBalance = 0.0
+  if (accounts.length) {
+    totalBalance = accounts.reduce(
+      (a, b) => parseFloat(a) + parseFloat(b.balance),
+      0,
+    )
+  } else {
+    totalBalance = 0.0
+  }
+  return totalBalance
+}
+module.exports = { isValidBudgetAndAccount, getTotalBalance }
