bdb4eaba50d87120e260e443e91bc8afd18a594a
osycon <fredrichawk@gmail.com>
2019-03-22 22:08:03 +0100

    refactor: Refactored accounts to use balancelog table.

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { isAuthenticated } = require('../middleware/authMiddleware')
 const db = require('../database/database.js')
+const { getAccountsWithBalance } = require('../helpers/helpers')
 
 const router = express.Router()
 
@@ -9,12 +10,13 @@ const router = express.Router()
 // Get all accounts
 router.get('/', isAuthenticated, async (req, res, next) => {
   const { userId } = req
-  // Get all accounts for that user.
-  const accounts = await db('accounts')
-    .select()
-    .where({ fk_user_id: userId })
 
-  res.json({ message: 'Accounts', accounts })
+  const accountsWithBalance = await getAccountsWithBalance(userId)
+
+  res.json({
+    message: 'Accounts',
+    accountsWithBalance,
+  })
 })
 
 // Create account
@@ -23,13 +25,21 @@ router.post('/', isAuthenticated, async (req, res, next) => {
   const { account, balance } = req.body
 
   // Create account
-  const newAccount = await db('accounts')
+  const [{ account_id }] = await db('accounts')
     .returning(['account_id'])
-    .insert({ fk_user_id: userId, account_name: account, balance })
-
+    .insert({ fk_user_id: userId, account_name: account })
+  const [newBalanceLog] = await db('balancelog')
+    .returning(['fk_account_id', 'balance', 'date', 'fk_user_id'])
+    .insert({
+      fk_user_id: userId,
+      balance,
+      fk_account_id: account_id,
+      date: new Date(),
+    })
   res.json({
     message: 'Accounts created',
-    newAccount,
+    account_id,
+    balance: newBalanceLog,
   })
 })
 
@@ -37,11 +47,11 @@ router.post('/', isAuthenticated, async (req, res, next) => {
 router.patch('/', isAuthenticated, async (req, res, next) => {
   const { userId } = req
   // Update account
-  const { accountId, accountName, balance } = req.body
-  const updatedAccount = await db('accounts')
-    .update({ account_name: accountName, balance })
+  const { accountId, accountName } = req.body
+  const [updatedAccount] = await db('accounts')
+    .update({ account_name: accountName })
     .where({ account_id: accountId, fk_user_id: userId })
-    .returning(['account_id', 'account_name', 'balance'])
+    .returning(['account_id', 'account_name'])
 
   res.json({
     message: 'Account updated',
@@ -53,6 +63,13 @@ router.patch('/', isAuthenticated, async (req, res, next) => {
 router.delete('/', isAuthenticated, async (req, res, next) => {
   const { userId } = req
   const { accountId } = req.body
+
+  await db('balancelog')
+    .del()
+    .where({ fk_account_id: accountId, fk_user_id: userId })
+  await db('transactions')
+    .del()
+    .where({ fk_account_id: accountId, fk_user_id: userId })
 
   const result = await db('accounts')
     .del()
