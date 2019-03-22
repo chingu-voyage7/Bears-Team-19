f6846457831f4cfcf7b4fb206714ba8efc57b93b
osycon <fredrichawk@gmail.com>
2019-03-22 22:08:32 +0100

    refactor: Added helpers for getting the balance with accounts.

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -15,17 +15,37 @@ const isValidBudgetAndAccount = async (accountId, budgetId, userId) => {
   return true
 }
 
-const getTotalBalance = async userId => {
-  // Get all accounts for that user
+const getAccountsWithBalance = async userId => {
+  // Get all accounts for that user.
   const accounts = await db('accounts')
     .select()
     .where({ fk_user_id: userId })
+  const accountsWithBalance = await Promise.all(
+    accounts.map(async account => {
+      const [{ balance, balancelog_id }] = await db('balancelog')
+        .select()
+        .where({ fk_user_id: userId, fk_account_id: account.account_id })
+        .orderBy('balancelog_id', 'desc')
+        .limit(1)
 
+      const accountWithBalance = {
+        ...account,
+        balance,
+        balancelog_id,
+      }
+      return accountWithBalance
+    }),
+  )
+  return accountsWithBalance
+}
+
+const getTotalBalance = async userId => {
   // Get all accounts for that user, if none, default to 0.00
   // Set total balance
+  const accountsWithBalance = await getAccountsWithBalance(userId)
   let totalBalance = 0.0
-  if (accounts.length) {
-    totalBalance = accounts.reduce(
+  if (accountsWithBalance.length) {
+    totalBalance = accountsWithBalance.reduce(
       (a, b) => parseFloat(a) + parseFloat(b.balance),
       0,
     )
@@ -34,4 +54,8 @@ const getTotalBalance = async userId => {
   }
   return totalBalance
 }
-module.exports = { isValidBudgetAndAccount, getTotalBalance }
+module.exports = {
+  isValidBudgetAndAccount,
+  getTotalBalance,
+  getAccountsWithBalance,
+}
