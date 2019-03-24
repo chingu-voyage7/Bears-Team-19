fbdda5ab27424cc1fb1227caca507743449acfad
osycon <fredrichawk@gmail.com>
2019-03-24 19:22:41 +0100

    refactor: Updated database to use account balance table and total balance table.

---
+++ server/migrations/20190324175923_totalbalance.js
@@ -0,0 +1,28 @@
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable('totalbalance', table => {
+    table
+      .increments('totalbalance_id')
+      .notNullable()
+      .primary()
+    table
+      .decimal('balance', 10, 2)
+      .notNullable()
+      .defaultTo(0.0)
+    table.string('date').notNullable()
+    table.integer('fk_user_id').notNullable()
+    table.integer('fk_account_id').notNullable()
+    table
+      .foreign('fk_user_id')
+      .references('user_id')
+      .inTable('users')
+    table
+      .foreign('fk_account_id')
+      .references('account_id')
+      .inTable('accounts')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTableIfExists('totalbalance')
+}
