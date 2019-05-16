861aa338bf68a72d6a23d132dba36d2459754326
osycon <fredrichawk@gmail.com>
2019-05-16 06:42:06 +0200

    refator: Made TransactionList selfcontained.

--- budget-app/src/containers/Transactions/Transactions.js
+++ budget-app/src/containers/Transactions/Transactions.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import TransactionList from '../../components/TransactionList'
-import { getUser } from '../../store/actions/userActions'
 
 class Transactions extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.auth.uid)
-  }
   render() {
     const {
       auth: { uid },
-      user: { currency },
     } = this.props
-    return (
-      <div>
-        {currency && uid && (
-          <TransactionList uid={uid} userCurrency={currency} />
-        )}
-      </div>
-    )
+    return <div>{uid && <TransactionList uid={uid} />}</div>
   }
 }
 
 const mapStateToProps = state => {
   return {
     auth: state.firebase.auth,
-    user: state.user,
-  }
-}
-const mapDispatchToProps = dispatch => {
-  return {
-    getUser: uid => dispatch(getUser(uid)),
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Transactions)
+export default connect(mapStateToProps)(Transactions)
